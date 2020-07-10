48828ba61969ab2f93c62e9c098ec931adafe37b
miguel.silva <miguel.silva@vilt-group.com>
2020-07-10 15:15:20 +0100

    add new features: old, new, detailed, day, verbose, bots

--- cmd/nginx-log-parser/main.go
+++ cmd/nginx-log-parser/main.go
@@ -4,28 +4,111 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
+	"time"
 
 	"github.com/silvagpmiguel/nginx-log-parser/pkg/info"
 )
 
-// ReadLog is a function that reads a specific log (File) given in the argument
-func ReadLog(file *os.File) (info.InfoMap, error) {
+func readLog(file *os.File, newFlag bool, oldFlag bool, botFlag bool, detailedFlag bool, verboseFlag bool, dayFlag bool, day string) (string, error) {
 	infoMap := make(info.InfoMap)
 	scanner := bufio.NewScanner(file)
+	bots := 0
+	totalViews := 0
+	newcomers := 0
+	existingUsers := 0
+	clientErrors := 0
+	str := ""
+	noFlags := !botFlag && !dayFlag && !newFlag && !oldFlag && !detailedFlag
+	onDay := ""
+
+	if dayFlag {
+		onDay = "on " + day
+	}
 
 	for scanner.Scan() {
-		err := info.GetLogInfo(infoMap, scanner.Text())
+		info, err := info.GetLogInfo(infoMap, scanner.Text())
 		if err != nil {
-			return infoMap, err
+			return "", err
+		}
+
+		if info.IP == "0" {
+			continue
+		}
+
+		if dayFlag {
+			layout := "02/01/2006:15:04:05 -0700"
+			startDay, _ := time.Parse(layout, day+":00:00:00 +0000")
+			endDay, _ := time.Parse(layout, day+":23:59:59 +0000")
+
+			if !(info.Date.After(startDay) && info.Date.Before(endDay)) {
+				continue
+			}
+		}
+
+		if info.IsBot {
+			bots++
+			if verboseFlag && (botFlag || detailedFlag) {
+				fmt.Println(info.String())
+			}
+		} else {
+			if info.IsNewUser {
+				newcomers++
+				if verboseFlag && (newFlag || detailedFlag || dayFlag) {
+					fmt.Println(info.String())
+				}
+			} else {
+				if info.IsClientError {
+					clientErrors++
+				} else {
+					existingUsers++
+					if verboseFlag && (oldFlag || detailedFlag || dayFlag) {
+						fmt.Println(info.String())
+					}
+				}
+			}
+			totalViews++
+			if verboseFlag && (noFlags) {
+				fmt.Println(info.String())
+			}
 		}
 	}
 
-	if len(infoMap) == 0 {
-		return nil, fmt.Errorf("Invalid log fields")
+	if verboseFlag {
+		str += "\n"
 	}
 
-	return infoMap, nil
+	if totalViews == 0 {
+		return str, fmt.Errorf("Invalid log fields")
+	}
+
+	if detailedFlag {
+		str += fmt.Sprintf("Detailed Information %s\n", onDay)
+		str += fmt.Sprintf("Number of bots: %d\n", bots)
+		str += fmt.Sprintf("Number of new users who accessed the site: %d\n", newcomers)
+		str += fmt.Sprintf("Number of users who already had accessed the site: %d\n", existingUsers)
+		str += fmt.Sprintf("Number of user error requests: %d\n", clientErrors)
+		str += fmt.Sprintf("Total number of user views: %d\n", totalViews)
+		return str, nil
+	}
+	if botFlag {
+		str += fmt.Sprintf("Found %d bots who accessed the site %s\n", bots, onDay)
+	}
+	if newFlag {
+		str += fmt.Sprintf("Found %d new users who accessed the site %s\n", totalViews, onDay)
+	}
+	if oldFlag {
+		str += fmt.Sprintf("Found %d users who already had accessed the site %s\n", existingUsers, onDay)
+	}
+	if noFlags {
+		str += fmt.Sprintf("Found %d users who accessed the site %s\n", totalViews, onDay)
+	}
+	if dayFlag {
+		str += fmt.Sprintf("Found %d users who accessed the site on %s\n", totalViews, day)
+	}
+
+	return str, nil
 }
 
 func printCommandsInfo() {
@@ -35,31 +118,64 @@ func printCommandsInfo() {
 		"Reads information from a nginx access log located at $LOGPATH\n\n",
 		"Provide LOGPATH as last argument\n\n",
 		"OPTIONS\n",
-		"\t-day,\t\tDisplay the number of users that accessed the website in a day <dd/mm/yyyy>\n",
-		"\t-bots,\t\tDisplay the number of bots that accessed the website\n",
+		"\t-bots,\t\tDisplay the number of bots who accessed the website\n",
+		"\t-day,\t\tDisplay the number of users who accessed the website in a day <dd/mm/yyyy>\n",
 		"\t-detailed,\tDisplay more detailed information\n",
+		"\t-new,\t\tDisplay the number of new users who accessed the website\n",
+		"\t-old,\t\tDisplay the number of users who had already accessed the website\n",
+		"\t-verbose,\tDisplay information about each line of the log\n",
 		"\t-h,\t\tDisplay this help and exit\n",
 		"EXAMPLE\n",
-		"\t./nginx-log-parser $LOGPATH,\tRead access log at $LOGPATH and display the total number of users that accessed a specific website",
+		"\t./nginx-log-parser $LOGPATH,\tRead access log at $LOGPATH and display the total number of users that accessed the website",
+		"\t./nginx-log-parser -day 09/07/2020 $LOGPATH,\tRead access log at $LOGPATH and display the number of users that accessed the website at that day",
+		"\t./nginx-log-parser -day 09/07/2020 $LOGPATH,\tRead access log at $LOGPATH and display the number of users that accessed the website at that day",
 	)
 }
 
 func main() {
 	filepath := ""
-	for i := 1; i < len(os.Args); i++ {
+	argsLen := len(os.Args)
+	dateRegex := regexp.MustCompile(`[0-9]{2}/[0-9]{2}/[0-9]{4}`)
+	botFlag := false
+	detailedFlag := false
+	newFlag := false
+	oldFlag := false
+	verboseFlag := false
+	dayFlag := false
+	day := ""
+
+	for i := 1; i < argsLen; i++ {
 		switch os.Args[i] {
 		case "-h":
 			printCommandsInfo()
 			return
 		case "-day":
-			return
+			if i+1 < argsLen {
+				if !dateRegex.MatchString(os.Args[i+1]) {
+					fmt.Println("Error. Insert a valid date <dd/mm/yyyy>")
+					return
+				}
+				dayFlag = true
+				day = os.Args[i+1]
+				i++
+			}
 		case "-bots":
-			return
+			botFlag = true
 		case "-detailed":
-			return
-		}
-		if strings.Contains(os.Args[i], ".log") {
-			filepath = os.Args[i]
+			detailedFlag = true
+		case "-old":
+			oldFlag = true
+		case "-new":
+			newFlag = true
+		case "-verbose":
+			verboseFlag = true
+		default:
+			if strings.Contains(os.Args[i], ".log") {
+				filepath = os.Args[i]
+			} else {
+				fmt.Println("Error. Wrong arguments")
+				return
+			}
 		}
 	}
 
@@ -75,13 +191,13 @@ func main() {
 		return
 	}
 
-	infoMap, err := ReadLog(file)
+	str, err := readLog(file, newFlag, oldFlag, botFlag, detailedFlag, verboseFlag, dayFlag, day)
 
 	if err != nil {
 		fmt.Printf("Error: Couldn't read log %s: %v\n", filepath, err)
 		return
 	}
 
-	fmt.Printf("Found %d users that visited your site!\n", len(infoMap))
+	fmt.Printf(str)
 
 }
--- pkg/info/info.go
+++ pkg/info/info.go
@@ -18,40 +18,76 @@ func init() {
 
 // Info represents a valid log entry
 type Info struct {
-	IP   string
-	Date time.Time
+	IP            string
+	Date          time.Time
+	IsBot         bool
+	IsNewUser     bool
+	IsClientError bool
 }
 
 // InfoMap represents an map with IP as key and Info as value
 type InfoMap map[string]Info
 
 // GetLogInfo parses the content of the log
-func GetLogInfo(infoMap InfoMap, str string) error {
+func GetLogInfo(infoMap InfoMap, str string) (Info, error) {
+	info := Info{IP: "0", IsBot: false, IsNewUser: false, IsClientError: false}
+
 	aux := strings.SplitN(str, " ", 2)
 	ip := aux[0]
 	_, ok := infoMap[ip]
 
 	if !ok && (strings.Contains(aux[0], ".") || strings.Contains(aux[0], ":")) {
 		maybeDate := DateRegex.FindString(aux[1])
+
 		if maybeDate != "" {
 			date, err := CreateDate(maybeDate)
 
 			if err != nil {
-				return err
+				return info, err
 			}
 
-			infoMap[ip] = Info{
-				IP:   ip,
-				Date: date,
+			botFlag := strings.Contains(aux[1], "wp-admin")
+			newFlag := strings.Contains(aux[1], "HTTP/1.1\" 200")
+			clientError := strings.Contains(aux[1], "HTTP/1.1\" 4")
+			info = Info{
+				IP:            ip,
+				Date:          date,
+				IsBot:         botFlag,
+				IsNewUser:     newFlag,
+				IsClientError: clientError,
 			}
+
+			infoMap[ip] = info
 		}
 	}
-
-	return nil
+	return info, nil
 }
 
 // CreateDate returns a time.Time value from "02/Jan/2006:15:04:05 -0700" as layout
 func CreateDate(str string) (time.Time, error) {
 	layout := "[02/Jan/2006:15:04:05 -0700]"
 	return time.Parse(layout, str)
+}
+
+func (i Info) String() string {
+	str := ""
+
+	if i.IP == "0" {
+		return str
+	}
+
+	if i.IsBot {
+		str += i.Date.String() + ": Found a bot -> " + i.IP
+	} else {
+		if i.IsNewUser {
+			str += i.Date.String() + ": Found a new user -> " + i.IP
+		} else {
+			if i.IsClientError {
+				str += i.Date.String() + ": Found a client error request -> " + i.IP
+			} else {
+				str += i.Date.String() + ": Found a user who had already accessed this site -> " + i.IP
+			}
+		}
+	}
+	return str
 }
